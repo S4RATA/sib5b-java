1af9980f8b3c893cd63800d42d8d5e1b11669c9b
S4RATA <miuw2711@gmail.com>
2024-10-09 15:03:05 +0800

    first commit

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        player player = new player();
+    }
+    
+}
---
+++ player.java
@@ -0,0 +1,22 @@
+public class player {
+    String namePlayer = "pregnant hippo 2711";
+    int x;
+    int y;
+    int health = 1;
+    int atack = 2;
+    int speed = 2;
+    int defense = 10;
+    boolean firstskill = true;
+    boolean secondskill = true;
+    boolean ultimateskill = true;
+    
+    public void GetInfoplayer(){
+        System.out.println("informasi Player \nNickname : " + namePlayer + "\nPosisi Player" + "\nx :" + x + "y :" + y + " \nhealth :" + health + "\nspeed :" + speed + "\natack :" + atack + " \ndefense :" + defense + " \nskillpertama : " + firstskill + " \nskill kedua :" + secondskill + "\nultimatesill : " + ultimateskill );
+        
+
+
+
+
+    }
+
+}
